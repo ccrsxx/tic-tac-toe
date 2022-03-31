a5d690c29120ab72a84dc3d6e7bf8cb60557d3bd
ccrsxx <aminrisal@gmail.com>
2022-03-31 21:32:02 +0700

    still adding things

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from 'react';
-import { Header, Footer } from './components';
+import { Header, Game, Footer } from './components';
 
 export function App() {
   return (
     <div className='App'>
       <Header />
+      <Game />
       <Footer />
     </div>
   );
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,3 +1,12 @@
 export function Game() {
-  return <main className='game-board'></main>;
+  return (
+    <main className='game'>
+      <div className='game-mode'>
+        <select name='mode' id='mode'>
+          <option value='normal'>Normal</option>
+          <option value='human'>Play against friend</option>
+        </select>
+      </div>
+    </main>
+  );
 }
