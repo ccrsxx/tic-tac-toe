dc7f308673950feb110b7cf8f373e36357d1668b
ccrsxx <aminrisal@gmail.com>
2022-04-01 21:50:37 +0700

    ui done, logic later

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,47 @@
 import { useState, useEffect } from 'react';
-import { Header, Game, Footer } from './components';
+import { Header, Footer } from './components';
 
 export function App() {
   return (
     <div className='App'>
       <Header />
-      <Game />
+      <main className='game'>
+        <div className='game-board'>
+          <div className='game-mode'>
+            <select name='mode' id='mode' className='mode'>
+              <option value='normal'>Normal</option>
+              <option value='human'>Play against friend</option>
+            </select>
+          </div>
+          <div className='game-score'>
+            <div className='player-card active'>
+              <p className='player-name'>X</p>
+              <p className='player-score'>2</p>
+            </div>
+            <div className='player-card'>
+              <p className='player-name'>O</p>
+              <p className='player-score'>_</p>
+            </div>
+          </div>
+          <p className='game-status'>Start game or select player</p>
+        </div>
+        <div className='game-container'>
+          <div className='square-container'>
+            <div className='square-box'></div>
+            <div className='square-box'></div>
+            <div className='square-box'></div>
+            <div className='square-box'></div>
+            <div className='square-box'></div>
+            <div className='square-box'></div>
+            <div className='square-box'></div>
+            <div className='square-box'></div>
+            <div className='square-box'></div>
+          </div>
+        </div>
+        <button className='restart-button' type='button'>
+          Restart game
+        </button>
+      </main>
       <Footer />
     </div>
   );
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,11 +1,23 @@
 export function Game() {
   return (
     <main className='game'>
-      <div className='game-mode'>
-        <select name='mode' id='mode'>
-          <option value='normal'>Normal</option>
-          <option value='human'>Play against friend</option>
-        </select>
+      <div className='game-board'>
+        <div className='game-mode'>
+          <select name='mode' id='mode' className='mode'>
+            <option value='normal'>Normal</option>
+            <option value='human'>Play against friend</option>
+          </select>
+        </div>
+        <div className='game-score'>
+          <div className='player-card active'>
+            <p className='player-name'>X</p>
+            <p className='player-score'>2</p>
+          </div>
+          <div className='player-card'>
+            <p className='player-name'>O</p>
+            <p className='player-score'>_</p>
+          </div>
+        </div>
       </div>
     </main>
   );
